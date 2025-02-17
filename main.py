8c022b2c14bb7b35f837f47c9c5f8b33803a1e33
jasperleung8 <jasperleung.lego@gmail.com>
2025-02-17 18:59:01 +0000

    Update main.py

--- main.py
+++ main.py
@@ -43,6 +43,82 @@ def reset_game():
 
 class Bird(pygame.sprite.Sprite):
 
-    def __init__(self,groups):
+    def __init__(self,x,y):
         pygame.sprite.Sprite.__init__(self)
-        
+        self.images = []
+        self.index = 0
+        self.counter = 0
+        for  num in range(1,4):
+            img = pygame.image.load(f"bird{num}.png")
+            self.images.append(img)
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x,y]
+        self.vel = 0
+        self.clicked = False
+
+    def update (self):
+
+        if flying == True:
+            self.vel += 0.5
+            if self.vel > 8:
+                self.vel = 8
+            if self.rect.bottom < 768:
+                self.rect.y += int(self.vel)
+
+        if game_over == False:
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False :
+                self.clicked = True
+                self.vel = -10
+            if pygame.mouse.get_pressed()[0] == 0:
+                self.clicked = False
+
+            flap_cooldown = 5
+            self.counter += 1
+
+            if self.counter > flap_cooldown:
+                self.counter = 0
+                self.index += 1
+                if self.index >= len(self.images):
+                    self.index = 0
+                self.image = self.images[self.index]
+
+            self.image = pygame.transform.rotate(self.images[self.index],self.vel * -2)
+
+        else:
+            self.image = pygame.transform.rotate(self.images[self.index],-90)
+
+class Pipe(pygame.sprite.Sprite):
+    
+    def __init__(self,x,y,position):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("pipe.png")
+        self.rect = self.image.get_rect()
+        if position == 1:
+            self.image = pygame.transform.flip(self.image,False,True)
+            self.rect.bottomleft = [x,y - int(pipe_gap / 2)] 
+        elif position == -1:
+            self.rect.topleft = [x,y + int(pipe_gap / 2)]
+
+    def update(self):
+        self.rect.x -= scroll_speed
+        if self.rect.right < 0 :
+            self.kill()
+
+class Button():
+    def __init__(self,x,y,image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+
+    def draw(self):
+        action = False
+
+        pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1:
+                action = True
+
+        screen.blit(self.image,(self.rect.x,self.rect.y))
+
+        return action
